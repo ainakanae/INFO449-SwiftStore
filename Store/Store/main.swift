ecbadcb68fe9b1384a56942692fee57f4bc8ebfa
Aina Engelbrekt <ainae@uw.edu>
2024-04-15 20:08:06 -0700

    done

--- Store/Store/main.swift
+++ Store/Store/main.swift
@@ -14,11 +14,77 @@ protocol SKU {
     
 }
 
-class Item {}
+class Item: SKU{
+    var name: String
+    var priceOfItem: Int
+    
+    
+    init(name: String, priceEach: Int) {
+        self.name = name
+        self.priceOfItem = priceEach
+    }
+    
+    func price() -> Int{
+        return self.priceOfItem
+    }
+}
 
-class Receipt {}
+class Receipt {
+    
+    var listOfItems: [SKU] = []
+    var subTotal: Int = 0
+    
+    func items() -> [SKU] {
+        return self.listOfItems
+    }
+    
+    
+    func output() -> String {
+        var finalReceipt = ""
+        for item in self.listOfItems {
+            finalReceipt += ("\n\(item.name): $\(Double(item.price()) / 100.0)")
+        }
+        let printReceipt = """
+Receipt:\(finalReceipt)
+------------------
+TOTAL: $\(Double(self.total()) / 100.0)
+"""
+        return printReceipt
+    }
+    
+    func addItem(item: SKU) {
+        self.listOfItems.append(item)
+        self.subTotal += item.price()
+    }
+    
+    
+    func total() -> Int {
+        return self.subTotal
+    }
+}
+    
 
-class Register {}
+
+class Register {
+    
+    var receipt: Receipt = Receipt()
+    
+    
+    func scan(_ item: SKU){
+        receipt.addItem(item: item)
+    }
+    
+    func subtotal() -> Int {
+        return receipt.total()
+    }
+    
+    func total() -> Receipt {
+           let tempReceipt = self.receipt
+           self.receipt = Receipt()
+           return tempReceipt
+       }
+    
+}
 
 class Store {
     let version = "0.1"
